2444b8fcd5aeb042fa81716493d0693f63e3b1af
William Bugenis <wbugenis@gmail.com>
2021-02-04 13:51:13 -0500

    commit tree 57bd5744a6404a1043605dcca4210a8913883153

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -9,9 +9,16 @@ class StudentsController < ApplicationController
   end
 
   def new
+    @student = Student.new
   end
 
   def create
+    @student = Student.create(student_params)
+    redirect_to student_path(@student)
+  end
+
+  def student_params
+    params.require(:student).permit(:first_name, :last_name)
   end
 
 end
